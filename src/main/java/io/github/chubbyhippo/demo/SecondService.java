9311fca3274c74fd64cb3c29e7a5390f34c04d84
chubbyhippo <mkmsacc@outlook.com>
2026-05-22 10:45:34 +0700

    Fix typo in SecondService getText method output

--- src/main/java/io/github/chubbyhippo/demo/SecondService.java
+++ src/main/java/io/github/chubbyhippo/demo/SecondService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecondService {
     public String getText() {
-        return "First Service";
+        return "Second Service";
     }
 }
