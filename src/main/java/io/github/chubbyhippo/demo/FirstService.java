f8f59a27d76ddfc0a4cad261f51895d07e76fdc2
chubbyhippo <mkmsacc@outlook.com>
2026-05-22 10:12:22 +0700

    Add FirstService with basic getText method

---
+++ src/main/java/io/github/chubbyhippo/demo/FirstService.java
@@ -0,0 +1,10 @@
+package io.github.chubbyhippo.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirstService {
+  public String getText() {
+      return "First Service";
+  }
+}
