4931dcca654726d1e5069a121886a4b41b075b35
chubbyhippo <mkmsacc@outlook.com>
2026-05-22 10:20:24 +0700

    Add ToggleController to demonstrate feature toggle-based logic

---
+++ src/main/java/io/github/chubbyhippo/demo/ToggleController.java
@@ -0,0 +1,30 @@
+package io.github.chubbyhippo.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.togglz.core.Feature;
+import org.togglz.core.manager.FeatureManager;
+import org.togglz.core.util.NamedFeature;
+
+@RestController
+public class ToggleController {
+    private final FeatureManager featureManager;
+    public static final Feature TOGGLE = new NamedFeature("TOGGLE");
+
+    private final FirstService firstService;
+    private final SecondService secondService;
+
+    public ToggleController(FeatureManager featureManager, FirstService firstService, SecondService secondService) {
+        this.featureManager = featureManager;
+        this.firstService = firstService;
+        this.secondService = secondService;
+    }
+
+    @GetMapping("/")
+    public String getText() {
+        if (featureManager.isActive(TOGGLE)) {
+            return firstService.getText();
+        }
+        return secondService.getText();
+    }
+}
